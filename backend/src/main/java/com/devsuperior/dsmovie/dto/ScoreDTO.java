206e917b014483dd56fea57ccb20cac9bbadbad1
Evaristo Ramalho <evaristo.ra@hotmail.com>
2022-06-19 19:07:10 -0300

    Save Score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dsmovie.dto;
+
+import lombok.Data;
+
+@Data
+public class ScoreDTO {
+    private Long movieId;
+    private String email;
+    private Double score;
+    
+}
