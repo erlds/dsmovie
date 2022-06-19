7dc973af4c948a67934ce2d12a9d68fa86c4c200
Evaristo Ramalho <evaristo.ra@hotmail.com>
2022-06-18 21:59:11 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dsmovie.dto;
+
+import com.devsuperior.dsmovie.entities.Movie;
+
+import lombok.Data;
+
+@Data
+public class MovieDTO {
+    private Long id;
+    private String title;
+    private Double score;
+    private Integer count;
+    private String image;
+
+    public MovieDTO(Movie movie){
+        id = movie.getId();
+        title = movie.getTitle();
+        score = movie.getScore();
+        count = movie.getCount();
+        image = movie.getImage();
+    }
+}
